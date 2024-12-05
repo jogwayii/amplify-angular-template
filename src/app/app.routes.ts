2aa9c4722a000042a2309f6b8b5c6231bf80da10
JO <jogwayi@amazon.com>
2024-12-05 15:48:15 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+  {
+    path: 'route1',
+    component: Route1Component
+  },  
+  {
+    path: 'route2',
+    component: Route2Component
+  }
+];
