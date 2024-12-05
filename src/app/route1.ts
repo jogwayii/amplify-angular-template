7cf89374ecbbc13a0904b3d52a77a09d69e7c8a3
JO <jogwayi@amazon.com>
2024-12-05 15:49:55 +0300

    Create route1.ts

---
+++ src/app/route1.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'route1',
+  templateUrl: './route1.html'
+})
+export class Route1Component {
+
+  
+  constructor() {
+
+  }
+}
