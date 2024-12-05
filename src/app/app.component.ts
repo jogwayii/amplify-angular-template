97ff9cf7c0b3f5f02d5581628ad10a097c2a0873
JO <jogwayi@amazon.com>
2024-12-05 15:52:23 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
+import { Route1Component } from './route1';
+import { Route2Component } from './route2';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 
@@ -11,7 +13,7 @@ Amplify.configure(outputs);
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterOutlet, TodosComponent, Route1Component, Route2Component],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
