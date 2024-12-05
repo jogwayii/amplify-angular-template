157abf55d5a5765e2905d2780fa5ba1f56e2bb5f
JO <jogwayi@amazon.com>
2024-12-05 15:50:23 +0300

    Create route2.ts

---
+++ src/app/route2.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'route2',
+  templateUrl: './route2.html'
+})
+export class Route2Component {
+
+  
+  constructor() {
+
+  }
+}
